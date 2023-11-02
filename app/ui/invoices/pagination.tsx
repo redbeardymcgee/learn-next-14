5e34ea89aac54408902364e1a98ac2633fc19176
redbeardy <redbeardymcgee@gmail.com>
2023-11-01 21:14:31 -0500

    Initial commit from Chapter 6

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,9 +1,9 @@
-'use client';
+'use client'
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { generatePagination } from '@/app/lib/utils'
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
@@ -49,7 +49,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
         />
       </div> */}
     </>
-  );
+  )
 }
 
 function PaginationNumber({
@@ -58,10 +58,10 @@ function PaginationNumber({
   isActive,
   position,
 }: {
-  page: number | string;
-  href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
-  isActive: boolean;
+  page: number | string
+  href: string
+  position?: 'first' | 'last' | 'middle' | 'single'
+  isActive: boolean
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center text-sm border',
@@ -72,15 +72,18 @@ function PaginationNumber({
       'hover:bg-gray-100': !isActive && position !== 'middle',
       'text-gray-300': position === 'middle',
     },
-  );
+  )
 
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
   ) : (
-    <Link href={href} className={className}>
+    <Link
+      href={href}
+      className={className}
+    >
       {page}
     </Link>
-  );
+  )
 }
 
 function PaginationArrow({
@@ -88,9 +91,9 @@ function PaginationArrow({
   direction,
   isDisabled,
 }: {
-  href: string;
-  direction: 'left' | 'right';
-  isDisabled?: boolean;
+  href: string
+  direction: 'left' | 'right'
+  isDisabled?: boolean
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center rounded-md border',
@@ -100,20 +103,23 @@ function PaginationArrow({
       'mr-2 md:mr-4': direction === 'left',
       'ml-2 md:ml-4': direction === 'right',
     },
-  );
+  )
 
   const icon =
     direction === 'left' ? (
-      <ArrowLeftIcon className="w-4" />
+      <ArrowLeftIcon className='w-4' />
     ) : (
-      <ArrowRightIcon className="w-4" />
-    );
+      <ArrowRightIcon className='w-4' />
+    )
 
   return isDisabled ? (
     <div className={className}>{icon}</div>
   ) : (
-    <Link className={className} href={href}>
+    <Link
+      className={className}
+      href={href}
+    >
       {icon}
     </Link>
-  );
+  )
 }
